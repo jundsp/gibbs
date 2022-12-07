ab894cf57af74ede5db57389ada7970d311aa0bb
jundsp <43224513+jundsp@users.noreply.github.com>
2022-12-06 21:41:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,146 +8,6 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
-class Module(object):
-    r'''
-    Gibbs module base class.
-
-    Author: Julian Neri, May 2022
-    '''
-    def __init__(self):
-        self._parameters = OrderedDict()
-
-    @property
-    def nparams(self):
-        return len(self._parameters)
-
-    # Used as a shortcut to get parameters : self._parameters['mu'] ==> self.mu
-    def __getattr__(self, name: str):
-        if '_parameters' in self.__dict__:
-            _parameters = self.__dict__['_parameters']
-            if name in _parameters:
-                return _parameters[name]
-        raise AttributeError("'{}' object has no attribute '{}'".format(type(self).__name__, name))
-
-    # Used to ensure that parameters are set explicity, and not overwritten.
-    def __setattr__(self, __name: str, __value) -> None:
-        if '_parameters' in self.__dict__:
-            if __name in self._parameters:
-                raise AttributeError("Parameter '{}' must be set with '._parameters[key] = value'".format(__name))          
-        self.__dict__[__name] = __value
-
-    def __dir__(self) -> Iterable[str]:
-        return list(self._parameters.keys())
-
-    def __repr__(self) -> str:
-        output = self.__class__.__name__  + " \n"
-        for i in self._parameters.keys():
-            output += " " + i +  " =  " + str(self._parameters[i]) + " \n"
-        return output
-
-
-
-class NormalParameters(Module):
-    r'''
-        Bayesian linear dynamical system parameters
-
-        Gibbs sampling. 
-
-        Author: Julian Neri, 2022
-    '''
-    def __init__(self,output_dim=1,input_dim=1,hyper_sample=True,system_covariance=True):
-        super(NormalParameters,self).__init__()
-        self._dimo = output_dim
-        self._dimi = input_dim
-        self.hyper_sample = hyper_sample
-        self.system_cov = system_covariance
-
-        self.initialize()
-
-    def __call__(self, y, x):
-        return self.forward(y,x)
-
-    @property
-    def output_dim(self):
-        return self._dimo
-    @output_dim.setter
-    def output_dim(self,value):
-        value = np.maximum(1,value)
-        if value != self._dimo:
-            self._dimo = value
-            self.initialize()
-
-    @property
-    def input_dim(self):
-        return self._dimi
-    @input_dim.setter
-    def input_dim(self,value):
-        value = np.maximum(1,value)
-        if value != self.dimi:
-            self._dimi = value
-            self.initialize()
-
-    def initialize(self):
-        A = np.eye(self.output_dim,self.input_dim)
-        A += np.random.normal(0,1e-3,A.shape)
-        A /= np.abs(A).sum(-1)[:,None]
-        Q = np.eye(self.output_dim)
-        alpha = np.ones((self.input_dim))*1e-1
-
-        self._parameters["A"] = A.copy()
-        self._parameters["Q"] = Q.copy()
-        self._parameters["alpha"] = alpha.copy()
-
-        nu = self.output_dim + 1.0
-        v = 1.0
-
-        self.a0 = nu / 2.0
-        self.b0 = 1.0 / (2*v)
-
-    def _sample_A(self):
-        tau = 1.0 / np.diag(self.Q)
-        Alpha = np.diag(self.alpha)
-        for row in range(self.output_dim):
-            ell = tau[row] * (self.y[:,row] @ self.x)
-            Lam = tau[row] *  ( (self.x.T @ self.x) + Alpha )
-            Sigma = la.inv(Lam)
-            mu = Sigma @ ell
-            self._parameters['A'][row] = mvn.rvs(mu,Sigma)
-
-    def _sample_Q(self):
-        Nk = self.N + self.input_dim
-        x_eps = self.y - self.x @ self.A.T
-        quad = np.diag(x_eps.T @ x_eps) + np.diag((self.A) @ np.diag(self.alpha) @ (self.A).T)
-
-        a_hat = self.a0 + 1/2 * Nk
-        b_hat = self.b0 + 1/2 * quad
-
-        tau = np.atleast_1d(gamma.rvs(a_hat,scale=1/b_hat))
-        self._parameters['Q'] = np.diag(1/tau)
-
-    def _sample_alpha(self):
-        if self.hyper_sample is False:
-            return 0
-        # The marginal prior over A is then a Cauchy distribution: N(a,|0,1/alpha)Gam(alpha|.5,.5) => Cauchy(a)
-        a0, b0 = .5,.5
-        a = np.zeros(self.input_dim)
-        b = np.zeros(self.input_dim)
-        tau = 1.0 / np.diag(self.Q)
-        for col in range(self.input_dim):
-            a[col] = a0 + 1/2*self.output_dim
-            b[col] = b0 + 1/2*( ((self.A[:,col]) ** 2.0)).sum(0)
-        self._parameters['alpha'] = np.atleast_1d(gamma.rvs(a,scale=1.0/b))
-
-    def forward(self,y,x):
-        self.y = y
-        self.x = x
-        self.N = y.shape[0]
-        self._sample_A()
-        self._sample_Q()
-        self._sample_alpha()
-        return self._parameters
-
-
 class Gibbs(object):
     r'''
     Gibbs sampler base class.
@@ -222,13 +82,181 @@ class Gibbs(object):
             self._append(_params)
         self.get_estimates(reduction='median',burn_rate=burn_rate)
     
-    
-N = 100
-x = np.ones((N,1))
-Sigma = wishart.rvs(2,np.eye(2))
-y = mvn.rvs(np.zeros(2),Sigma,N)
 
-model = NormalParameters(output_dim=2,input_dim=1)
+
+class Module(object):
+    r'''
+    Gibbs module base class.
+
+    Author: Julian Neri, May 2022
+    '''
+    def __init__(self):
+        self._parameters = OrderedDict()
+
+    @property
+    def nparams(self):
+        return len(self._parameters)
+
+    # Used as a shortcut to get parameters : self._parameters['mu'] ==> self.mu
+    def __getattr__(self, name: str):
+        if '_parameters' in self.__dict__:
+            _parameters = self.__dict__['_parameters']
+            if name in _parameters:
+                return _parameters[name]
+        raise AttributeError("'{}' object has no attribute '{}'".format(type(self).__name__, name))
+
+    # Used to ensure that parameters are set explicity, and not overwritten.
+    def __setattr__(self, __name: str, __value) -> None:
+        if '_parameters' in self.__dict__:
+            if __name in self._parameters:
+                raise AttributeError("Parameter '{}' must be set with '._parameters[key] = value'".format(__name))          
+        self.__dict__[__name] = __value
+
+    def __dir__(self) -> Iterable[str]:
+        return list(self._parameters.keys())
+
+    def __repr__(self) -> str:
+        output = self.__class__.__name__  + " \n"
+        for i in self._parameters.keys():
+            output += " " + i +  " =  " + str(self._parameters[i]) + " \n"
+        return output
+
+class NormalWishart(Module):
+    r'''
+        Normal wishart system parameters
+
+        Gibbs sampling. 
+
+        Author: Julian Neri, 2022
+    '''
+    def __init__(self,output_dim=1,input_dim=1,hyper_sample=True,full_covariance=True):
+        super(NormalWishart,self).__init__()
+        self._dimo = output_dim
+        self._dimi = input_dim
+        self.hyper_sample = hyper_sample
+        self.full_covariance = full_covariance
+
+        self.initialize()
+
+    def __call__(self, y, x):
+        return self.forward(y,x)
+
+    @property
+    def output_dim(self):
+        return self._dimo
+    @output_dim.setter
+    def output_dim(self,value):
+        value = np.maximum(1,value)
+        if value != self._dimo:
+            self._dimo = value
+            self.initialize()
+
+    @property
+    def input_dim(self):
+        return self._dimi
+    @input_dim.setter
+    def input_dim(self,value):
+        value = np.maximum(1,value)
+        if value != self.dimi:
+            self._dimi = value
+            self.initialize()
+
+    def initialize(self):
+        self.define_priors()
+        self.initialize_parameters()
+        
+    def define_priors(self):
+        self.nu0 = self.output_dim + 1.0
+        self.iW0 = np.eye(self.output_dim)
+
+        self.a0 = self.nu0 / 2.0
+        self.b0 = 1.0 / (2)
+
+        self.c0 = .5
+        self.d0 = .5
+        
+    def initialize_parameters(self):
+        A = np.eye(self.output_dim,self.input_dim)
+        A += np.random.normal(0,1e-3,A.shape)
+        A /= np.abs(A).sum(-1)[:,None]
+        Q = np.eye(self.output_dim)
+        alpha = np.ones((self.input_dim))*1e-1
+
+        self._parameters["A"] = A.copy()
+        self._parameters["Q"] = Q.copy()
+        self._parameters["alpha"] = alpha.copy()
+
+    def _sample_A(self):
+        tau = 1.0 / np.diag(self.Q)
+        Alpha = np.diag(self.alpha)
+        for row in range(self.output_dim):
+            ell = tau[row] * (self.y[:,row] @ self.x)
+            Lam = tau[row] *  ( (self.x.T @ self.x) + Alpha )
+            Sigma = la.inv(Lam)
+            mu = Sigma @ ell
+            self._parameters['A'][row] = mvn.rvs(mu,Sigma)
+
+    def _sample_alpha(self):
+        if self.hyper_sample is False:
+            return 0
+        # The marginal prior over A is then a Cauchy distribution: N(a,|0,1/alpha)Gam(alpha|.5,.5) => Cauchy(a)
+        a = np.zeros(self.input_dim)
+        b = np.zeros(self.input_dim)
+        for col in range(self.input_dim):
+            a[col] = self.c0 + 1/2*self.output_dim
+            b[col] = self.d0 + 1/2*( ((self.A[:,col]) ** 2.0)).sum(0)
+        self._parameters['alpha'] = np.atleast_1d(gamma.rvs(a,scale=1.0/b))
+
+    def _sample_Q(self):
+        if self.full_covariance:
+            Q = self._sample_cov_full()
+        else:
+            Q = self._sample_cov_diag()
+        self._parameters['Q'] = Q
+
+    def _sample_cov_diag(self):
+        Nk = self.N + self.input_dim
+        x_eps = self.y - self.x @ self.A.T
+        quad = np.diag(x_eps.T @ x_eps) + np.diag((self.A) @ np.diag(self.alpha) @ (self.A).T)
+
+        a_hat = self.a0 + 1/2 * Nk
+        b_hat = self.b0 + 1/2 * quad
+
+        tau = np.atleast_1d(gamma.rvs(a_hat,scale=1/b_hat))
+        return np.diag(1/tau)
+
+    def _sample_cov_full(self):
+        nu = self.nu0 + self.N
+
+        y_hat = (self.x @ self.A.T)
+        x_eps = (self.y - y_hat)
+        quad = x_eps.T @ x_eps
+        iW = self.iW0 + quad
+        iW = .5*(iW + iW.T)
+        W = la.inv(iW)
+
+        Lambda = np.atleast_2d(wishart.rvs(df=nu,scale=W) )
+        return la.inv(Lambda)
+        
+    def forward(self,y,x):
+        self.y = y
+        self.x = x
+        self.N = y.shape[0]
+        self._sample_A()
+        self._sample_Q()
+        self._sample_alpha()
+        return self._parameters
+
+
+
+
+N = 500
+x = np.ones((N,1))
+mu = np.zeros(2)+10
+Sigma = wishart.rvs(3,np.eye(2))
+y = mvn.rvs(mu,Sigma,N)
+
+model = NormalWishart(output_dim=2,input_dim=1,full_covariance=True)
 
 sampler = Gibbs()
 sampler.fit((y,x),model,1000)
@@ -245,8 +273,8 @@ plt.tight_layout()
 
 
 plt.figure()
-plt.scatter(y[:,0],y[:,1])
-mu = sampler._estimates['A']
-Sigma = sampler._estimates['Q']
-plot_cov_ellipse(pos=mu,cov=Sigma,nstd=1,fill=None)
-plt.axis("equal")
+plt.scatter(y[:,0],y[:,1],s=10,alpha=.5)
+mu_hat = sampler._estimates['A']
+Sigma_hat = sampler._estimates['Q']
+plot_cov_ellipse(pos=mu,cov=Sigma,nstd=1,fill=None,color='b',linewidth=2)
+plot_cov_ellipse(pos=mu_hat,cov=Sigma_hat,nstd=1,fill=None,color='r',linewidth=2)
