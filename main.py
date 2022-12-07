8e9294dbcdb13c220efe1a23fdcfbc92918db00d
jundsp <43224513+jundsp@users.noreply.github.com>
2022-12-06 23:24:23 -0500

    module and mixture

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from gibbs import get_mean, get_median, plot_cov_ellipse
+#%%
+from gibbs import get_mean, get_median, plot_cov_ellipse, gmm_generate
 import numpy as np
 from scipy.stats import multivariate_normal as mvn
-from scipy.stats import gamma, wishart
+from scipy.stats import gamma, wishart, dirichlet
 import scipy.linalg as la
-from typing import OrderedDict, Optional, Iterable
+from scipy.special import logsumexp
+from typing import OrderedDict, Optional, Iterable, overload
+from itertools import islice
+import operator
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
 
 class Gibbs(object):
     r'''
@@ -18,41 +21,25 @@ class Gibbs(object):
         self._parameters = OrderedDict()
         self._samples = OrderedDict()
         self._estimates = OrderedDict()
-        self._modules = OrderedDict()
 
     @property
     def nparams(self):
-        return len(self._parameters)
+        return len(self._estimates)
 
     def __dir__(self) -> Iterable[str]:
-        return list(self._parameters.keys())
+        return list(self._estimates.keys())
 
     def __repr__(self) -> str:
         output = self.__class__.__name__  + " \n"
         for i in self._estimates.keys():
-            output += " " + i +  " =  " + str(self._parameters[i]) + " \n"
+            output += " " + i +  " =  " + str(self._estimates[i]) + " \n"
         return output
-
-    def add_module(self, name: str, module: Optional['object']) -> None:
-        if not isinstance(module, object) and module is not None:
-            raise TypeError("{} is not a object subclass".format(
-                module))
-        elif hasattr(self, name) and name not in self._modules:
-            raise KeyError("attribute '{}' already exists".format(name))
-        elif '.' in name:
-            raise KeyError("module name can't contain \".\", got: {}".format(name))
-        elif name == '':
-            raise KeyError("module name can't be empty string \"\"")
-        self._modules[name] = module
 
     def _append(self,params):
         for p in params:
             if p not in self._samples.keys():
                 self._samples[p] = []
             self._samples[p].append(params[p].copy())
-
-        for m in self._modules:
-            self._modules[m]._append()
 
     def get_estimates(self,reduction='median',burn_rate=.75,skip_rate=1):
         if reduction == 'median':
@@ -78,12 +65,10 @@ class Gibbs(object):
 
     def fit(self,data,model,samples=100,burn_rate=.75):
         for s in tqdm(range(samples)):
-            _params = model(*data)
+            _params = model(data)
             self._append(_params)
         self.get_estimates(reduction='median',burn_rate=burn_rate)
     
-
-
 class Module(object):
     r'''
     Gibbs module base class.
@@ -92,10 +77,23 @@ class Module(object):
     '''
     def __init__(self):
         self._parameters = OrderedDict()
+        self._modules = OrderedDict()
 
     @property
     def nparams(self):
         return len(self._parameters)
+
+    def add_module(self, name: str, module: Optional['Module']) -> None:
+        if not isinstance(module, Module) and module is not None:
+            raise TypeError("{} is not a Module subclass".format(
+                module))
+        elif hasattr(self, name) and name not in self._modules:
+            raise KeyError("attribute '{}' already exists".format(name))
+        elif '.' in name:
+            raise KeyError("module name can't contain \".\", got: {}".format(name))
+        elif name == '':
+            raise KeyError("module name can't be empty string \"\"")
+        self._modules[name] = module
 
     # Used as a shortcut to get parameters : self._parameters['mu'] ==> self.mu
     def __getattr__(self, name: str):
@@ -103,23 +101,106 @@ class Module(object):
             _parameters = self.__dict__['_parameters']
             if name in _parameters:
                 return _parameters[name]
+        if '_modules' in self.__dict__:
+            modules = self.__dict__['_modules']
+            if name in modules:
+                return modules[name]
         raise AttributeError("'{}' object has no attribute '{}'".format(type(self).__name__, name))
 
     # Used to ensure that parameters are set explicity, and not overwritten.
     def __setattr__(self, __name: str, __value) -> None:
         if '_parameters' in self.__dict__:
             if __name in self._parameters:
-                raise AttributeError("Parameter '{}' must be set with '._parameters[key] = value'".format(__name))          
-        self.__dict__[__name] = __value
+                raise AttributeError("Parameter '{}' must be set with '._parameters[key] = value'".format(__name))
+
+        modules = self.__dict__.get('_modules')
+        if isinstance(__value, Module):
+            if modules is None:
+                raise AttributeError(
+                    "cannot assign module before Module.__init__() call")
+            modules[__name] = __value
+        elif modules is not None and __name in modules:
+            if __value is not None:
+                raise TypeError("cannot assign as child module (Module or None expected)")
+            modules[__name] = __value
+        else:
+            self.__dict__[__name] = __value
 
     def __dir__(self) -> Iterable[str]:
         return list(self._parameters.keys())
 
-    def __repr__(self) -> str:
-        output = self.__class__.__name__  + " \n"
-        for i in self._parameters.keys():
-            output += " " + i +  " =  " + str(self._parameters[i]) + " \n"
-        return output
+    def _get_name(self):
+        return self.__class__.__name__
+
+    def __repr__(self):
+        child_lines = []
+        for key, module in self._modules.items():
+            mod_str = repr(module)
+            child_lines.append('(' + key + '): ' + mod_str)
+        lines = child_lines
+
+        main_str = self._get_name() + '('
+        if lines:
+            main_str += '\n  ' + '\n  '.join(lines) + '\n'
+
+        main_str += ')'
+        return main_str
+
+    def __call__(self, *args, **kwds):
+        return self.forward(*args, **kwds)
+
+
+
+class Mixture(Module):
+    @overload
+    def __init__(self, *args: Module) -> None:
+        ...
+
+    @overload
+    def __init__(self, arg: 'OrderedDict[str, Module]') -> None:
+        ...
+    def __init__(self, *args):
+        super(Mixture, self).__init__()
+        if len(args) == 1 and isinstance(args[0], OrderedDict):
+            for key, module in args[0].items():
+                self.add_module(key, module)
+        else:
+            for idx, module in enumerate(args):
+                self.add_module(str(idx), module)
+
+    def __len__(self) -> int:
+        return len(self._modules)
+
+    def _get_item_by_idx(self, iterator, idx):
+        """Get the idx-th item of the iterator"""
+        size = len(self)
+        idx = operator.index(idx)
+        if not -size <= idx < size:
+            raise IndexError('index {} is out of range'.format(idx))
+        idx %= size
+        return next(islice(iterator, idx, None))
+
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            return self.__class__(OrderedDict(list(self._modules.items())[idx]))
+        else:
+            return self._get_item_by_idx(self._modules.values(), idx)
+
+    def append(self, module: Module) -> 'Mixture':
+        r"""Appends a given module to the end.
+
+        Args:
+            module (Module): module to append
+        """
+        self.add_module(str(len(self)), module)
+        return self
+
+    def forward(self,y,labels):
+        for k,m in enumerate(self._modules):
+            idx = labels == k
+            params = self._modules[m](y[idx])
+        return params
+
 
 class NormalWishart(Module):
     r'''
@@ -137,9 +218,6 @@ class NormalWishart(Module):
         self.full_covariance = full_covariance
 
         self.initialize()
-
-    def __call__(self, y, x):
-        return self.forward(y,x)
 
     @property
     def output_dim(self):
@@ -238,8 +316,11 @@ class NormalWishart(Module):
         Lambda = np.atleast_2d(wishart.rvs(df=nu,scale=W) )
         return la.inv(Lambda)
         
-    def forward(self,y,x):
+    def forward(self,y,x=None):
         self.y = y
+
+        if x is None:
+            x = np.ones((y.shape[0],self.input_dim))
         self.x = x
         self.N = y.shape[0]
         self._sample_A()
@@ -250,17 +331,91 @@ class NormalWishart(Module):
 
 
 
-N = 500
-x = np.ones((N,1))
-mu = np.zeros(2)+10
-Sigma = wishart.rvs(3,np.eye(2))
-y = mvn.rvs(mu,Sigma,N)
+class GMM(Module):
+    r'''
+        Finite Bayesian mixture of Gaussians.
 
-model = NormalWishart(output_dim=2,input_dim=1,full_covariance=True)
+        Gibbs sampling. 
 
+        Author: Julian Neri, 2022
+    '''
+    def __init__(self,output_dim=1,components=3):
+        super().__init__()
+        self.output_dim = output_dim
+        self.components = components
+
+        self._parameters["z"] = None
+        self._parameters["pi"] = np.ones(components)/components
+        
+        self.alpha0 = np.ones(components) / components
+        # Register these into the parameter list in sub-parameters, and make it more elegant to use. Use register module
+        self.sub_mod = [NormalWishart(output_dim=output_dim) for i in range(components)]
+        for k,sub in enumerate(self.sub_mod):
+            for p in sub._parameters:
+                key = str(p) + "_" + str(k+1)
+                self._parameters[key] = sub._parameters[p]
+
+        self.theta = Mixture(*[NormalWishart(output_dim=output_dim) for i in range(components)])
+
+    def loglikelihood(self,x):
+        N = x.shape[0]
+        loglike = np.zeros((N,self.components))
+        for k in range(self.components):
+            loglike[:,k] = mvn.logpdf(x,self.sub_mod[k].A.ravel(),self.sub_mod[k].Q)
+        return loglike
+
+    def _sample_z(self):
+        rho = self.loglikelihood(self.y) + np.log(self._parameters['pi'])
+        rho -= logsumexp(rho,-1).reshape(-1,1)
+        rho = np.exp(rho)
+        rho /= rho.sum(-1).reshape(-1,1)
+        for n in range(self.N):
+            self._parameters['z'][n] = np.random.multinomial(1,rho[n]).argmax()
+
+    def _sample_pi(self):
+        alpha = np.zeros(self.components)
+        for k in range(self.components):
+            alpha[k] = (self._parameters['z']==k).sum() + self.alpha0[k]
+        self._parameters['pi'] = dirichlet.rvs(alpha)
+
+    def forward(self,y):
+        # Parameters to mirror the structure of the modules.
+        self.y = y
+        self.N = y.shape[0]
+        if self._parameters['z'] is None:
+            self._parameters['z'] = np.random.randint(0,self.components,self.N)
+
+        self._sample_z()
+        self._sample_pi()
+        self.theta(y,self.z)
+
+        for k,sub in enumerate(self.sub_mod):
+            params = sub(y=y[self.z==k])
+            for p in params:
+                key = str(p) + "_" + str(k+1)
+                self._parameters[key] = params[p]
+        return self._parameters
+
+
+#%%
+temp = OrderedDict(m1 = NormalWishart(output_dim=2), m2 = NormalWishart(output_dim=2))
+mix = Mixture(temp)
+mix.append(NormalWishart(2))
+print(mix)
+
+N = 100
+np.random.seed(123)
+y = gmm_generate(n=N,n_components=2)[0]
+
+#%%
+model = GMM(output_dim=2,components=3)
 sampler = Gibbs()
-sampler.fit((y,x),model,1000)
+print(model)
+#%%
+sampler.fit(data=y,model=model,samples=100)
 
+
+#%%
 chain = sampler.get_chain(flatten=True)
 
 fig,ax = plt.subplots(len(chain),figsize=(5,len(chain)*1.5))
@@ -271,10 +426,17 @@ for j,s in enumerate(chain):
 ax[-1].set_xlabel('step')
 plt.tight_layout()
 
+#%%
+colors = np.array(['r','g','b','k','m','orange','yellow','brown']*30)
+plt.figure(figsize=(5,4))
+z_hat = sampler._estimates['z']
+kwds_scatter = dict(s=15,alpha=.5,edgecolor='none')
+plt.scatter(y[:,0],y[:,1],c=colors[z_hat],**kwds_scatter)
 
-plt.figure()
-plt.scatter(y[:,0],y[:,1],s=10,alpha=.5)
-mu_hat = sampler._estimates['A']
-Sigma_hat = sampler._estimates['Q']
-plot_cov_ellipse(pos=mu,cov=Sigma,nstd=1,fill=None,color='b',linewidth=2)
-plot_cov_ellipse(pos=mu_hat,cov=Sigma_hat,nstd=1,fill=None,color='r',linewidth=2)
+for k in np.unique(z_hat):
+    mu_hat = sampler._estimates['A_'+str(k+1)]
+    Sigma_hat = sampler._estimates['Q_'+str(k+1)]
+    plot_cov_ellipse(pos=mu_hat,cov=Sigma_hat,nstd=1,fill=None,color=colors[k],linewidth=2)
+plt.tight_layout()
+
+# %%
