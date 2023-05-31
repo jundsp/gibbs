bug fixed hmm normalization

gibbs/modules/hmm.py
@@ -2,14 +2,13 @@ import numpy as np
 
 from scipy.stats import multivariate_normal as mvn
 from scipy.stats import dirichlet
-from scipy.special import logsumexp
 from .module import Module 
 from .parameters import NormalWishart
 from .plate import Plate
 from ..dataclass import Data
+from ..utils import log_normalize
 
 np.seterr(all='ignore')
-
 
 #* Parameters should have a "sample /  learn" setting do register into the sampler. If not, then dont add to the chain, and allow for easy setting.
 
@@ -80,17 +79,18 @@ class HMM(Module):
         prediction = np.log(self._parameters['pi']).reshape(1,-1)
         for t in range(self.T):
             alpha[t] = logl[t] + prediction
-            c[t] = logsumexp(alpha[t])
-            alpha[t] -= c[t]
+            alpha[t], c[t] = log_normalize(alpha[t])
             prediction = self._predict_hmm(alpha[t])
         return alpha
-
+    
     def _backward_hmm(self,log_alpha:np.ndarray):
-        beta = log_alpha[-1] - logsumexp(log_alpha[-1])
-        self._parameters['z'][-1] = np.random.multinomial(1,np.exp(beta)).argmax()
+        beta = log_alpha[-1].copy()
+        beta, c = log_normalize(beta)
+        beta = np.exp(beta)            
+        self._parameters['z'][-1] = np.random.multinomial(1,beta).argmax()
         for t in range(self.T-2,-1,-1):
             beta = self.log_Gamma[:,self.z[t+1]] + log_alpha[t]
-            beta -= logsumexp(beta)
+            beta, c = log_normalize(beta)
             beta = np.exp(beta)
             self._parameters['z'][t] = np.random.multinomial(1,beta).argmax()
         

gibbs/utils.py
@@ -5,7 +5,7 @@ from scipy.stats import multinomial, wishart
 from scipy.stats import multivariate_normal as mvn, multivariate_t as mvt, wishart
 import scipy.linalg as la
 from scipy.optimize import linear_sum_assignment
-from scipy.special import gammaln
+from scipy.special import gammaln, logsumexp
 
 def plot_cov_ellipse(pos,cov, nstd=2, ax=None, fill=None, **kwargs):
     """
@@ -215,6 +215,15 @@ def relabel(probs,iters=10,verbose=False):
             break
 
     return tau
+
+
+def log_normalize(alpha):
+    c = logsumexp(alpha)
+    if np.isinf(c):
+        alpha = np.zeros_like(alpha)
+        c = logsumexp(alpha)
+    alpha -= c
+    return alpha, c
 
 
 def gamma_moments2params(mu,var):